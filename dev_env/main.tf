e5a5f350a907112130dc37788a47466e54adc82d
Shruti Bawase <shrutibawase01@gmail.com>
2025-08-02 22:36:39 +0530

    add new rg in canada

--- dev_env/main.tf
+++ dev_env/main.tf
@@ -3,3 +3,9 @@ module "resource_group" {
   resource_group_name     = "rg-todo-app"
   resource_group_location = "Central India"
 }
+
+module "resource_group-canda" {
+  source = "../../module/azuerm_resouce_group"
+  resource_group_name = "rg-todo-app-canda"
+  resource_hroup_location = "central canada"
+}
