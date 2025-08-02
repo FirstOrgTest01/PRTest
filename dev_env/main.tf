d3f95e639597fabecc9e407b5e1799cef4bc60e4
Shruti Bawase <shrutibawase01@gmail.com>
2025-08-02 20:42:06 +0530

    add comment in the dev_env/main.tf

--- dev_env/main.tf
+++ dev_env/main.tf
@@ -1,3 +1,4 @@
+# This module creates an Azure Resource Group for the TODO application environment
 module "resource_group" {
   source                  = "../module/azuerm_resouce_group"
   resource_group_name     = "rg-todo-app"
